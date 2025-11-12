b4409865cdb53a1e76fe8a35618e1e73cbb740f5
mariooooo3 <afrasineimario@gmail.com>
2025-11-12 20:56:50 +0200

    notification update

--- src/Visitor/NotificationVisitor.java
+++ src/Visitor/NotificationVisitor.java
@@ -4,12 +4,18 @@ import Entitati.Driver;
 import Entitati.Rider;
 
 public class NotificationVisitor implements Visitor {
+    String messageDriver = "!!!EXCLUSIV IN ACEASTA SAPTAMANA" +
+            " fiecare sofer primeste un bonus de 20%!!!";
     public void visit(Driver d) {
-        System.out.println("!!!EXCLUSIV IN ACEASTA SAPTAMANA" +
-                " fiecare sofer primeste un bonus de 20%!!!");
+        System.out.println(messageDriver);
     }
 
     public void visit(Rider r) {
         System.out.println("Bune venit, primele 3 curse au discount 20%, nu rata oferta!!");
+    }
+
+    public String getMessageDriver()
+    {
+        return messageDriver;
     }
 }
